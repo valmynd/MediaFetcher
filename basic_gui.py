e8e3a6e42b2f51b2aa3982abca60a7b9795b5367
rrae <rrae@localhorst.(none)>
2013-04-02 19:53:16 +0200

    slight changes on initial basic GUI

--- basic_gui.py
+++ basic_gui.py
@@ -1,9 +1,11 @@
 ___license___ = "GPL v3"
-from PySide import QtCore, QtGui
+from PySide.QtCore import *
+from PySide.QtGui import *
 
-class MainWindow(QtGui.QMainWindow):
+
+class MainWindow(QMainWindow):
 	def __init__(self):
-		QtGui.QMainWindow.__init__(self)
+		QMainWindow.__init__(self)
 		self.createActions()
 		self.createMenus()
 		self.createToolBars()
@@ -11,33 +13,50 @@ class MainWindow(QtGui.QMainWindow):
 		self.setWindowTitle("Media Fetcher")
 		self.resize(600, 400)
 		self.statusBar()
+		# monitor Clipboard
+		QObject.connect(QApplication.clipboard(), SIGNAL("dataChanged()"), self,
+							   SLOT("clipBoardChanged()"))
+
+	@Slot()
+	def clipBoardChanged(self):
+		if not QApplication.clipboard().mimeData().hasText(): return
+		text = QApplication.clipboard().text()
+		if '//' in text: # contains URL
+			QMessageBox.information(None, "ClipBoard", text);
+
 
 	def open(self):
-		fileName = QtGui.QFileDialog.getOpenFileName(self, "Open File", QtCore.QDir.currentPath())
+		fileName = QFileDialog.getOpenFileName(self, "Open File", QDir.currentPath())
 		if fileName:
 			pass
 
 	def about(self):
-		QtGui.QMessageBox.about(self, "About Media Fetcher", "Text")
+		QMessageBox.about(self, "About Media Fetcher", "Text")
 
 	def toggleStatusBar(self):
 		self.statusBar().show() if self.statusBar().isHidden() else self.statusBar().hide()
 
 	def createActions(self):
-		self.openAction = QtGui.QAction("&Open...", self, shortcut="Ctrl+O", triggered=self.open)
-		self.startAction = QtGui.QAction("Start All", self, triggered=self.open)
-		self.pauseAction = QtGui.QAction("&Pause All", self, triggered=self.open)
-		self.exitAction = QtGui.QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
-		self.toggleStatusBarAction = QtGui.QAction("&Statusbar", self, checkable=True, checked=True,
+		self.openAction = QAction("&Open...", self, shortcut="Ctrl+O", triggered=self.open)
+		self.startAction = QAction("&Start All", self, triggered=self.open)
+		self.pauseAction = QAction("&Pause All", self, triggered=self.open)
+		self.exitAction = QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
+		self.toggleStatusBarAction = QAction("Statusbar", self, checkable=True, checked=True,
 												   triggered=self.toggleStatusBar)
-		self.aboutAction = QtGui.QAction("&About", self, triggered=self.about)
-		#print(QtGui.QIcon.hasThemeIcon("edit-undo"))
-		#self.openAction.setIcon(QtGui.QIcon.fromTheme("edit-undo", QtGui.QIcon("/home/rrae/src/inspirations/pyside-examples/examples/widgets/icons/images/monkey_on_16x16.png")))
-		#self.openAction.setToolButtonStyle(QtCore.Qt.ToolButtonTextUnderIcon)
+		self.settingsAction = QAction("Prefere&nces", self, triggered=self.open)
+		self.aboutAction = QAction("About", self, triggered=self.about)
+		self.searchAction = QAction("Search", self, triggered=self.about)
+
+		self.openAction.setIcon(QIcon.fromTheme("folder-open"))
+		self.startAction.setIcon(QIcon.fromTheme("media-playback-start"))
+		self.pauseAction.setIcon(QIcon.fromTheme("media-playback-pause"))
+		self.settingsAction.setIcon(QIcon.fromTheme("emblem-system"))
+		self.exitAction.setIcon(QIcon.fromTheme("application-exit"))
+		self.searchAction.setIcon(QIcon.fromTheme("system-search"))
 		self.exitAction.setStatusTip('Exit application')
 
 	def createMenus(self):
-		self.fileMenu = QtGui.QMenu("&File", self)
+		self.fileMenu = QMenu("&File", self)
 		self.fileMenu.addAction(self.openAction)
 		self.fileMenu.addSeparator()
 		self.fileMenu.addAction(self.startAction)
@@ -45,32 +64,83 @@ class MainWindow(QtGui.QMainWindow):
 		self.fileMenu.addSeparator()
 		self.fileMenu.addAction(self.exitAction)
 
-		self.viewMenu = QtGui.QMenu("&View", self)
+		self.editMenu = QMenu("&Edit", self)
+		self.editMenu.addAction(self.settingsAction)
+
+		self.viewMenu = QMenu("&View", self)
 		self.viewMenu.addAction(self.toggleStatusBarAction)
 
-		self.helpMenu = QtGui.QMenu("&Help", self)
+		self.helpMenu = QMenu("&Help", self)
 		self.helpMenu.addAction(self.aboutAction)
 
 		self.menuBar().addMenu(self.fileMenu)
+		self.menuBar().addMenu(self.editMenu)
 		self.menuBar().addMenu(self.viewMenu)
 		self.menuBar().addMenu(self.helpMenu)
 
 	def createToolBars(self):
-		self.searchBar = QtGui.QLineEdit()
+		# http://aseigo.blogspot.de/2006/08/sweep-sweep-sweep-ui-floor.html
+		self.searchBar = QLineEdit()
 		self.toolBar = self.addToolBar("Search")
 		self.toolBar.addAction(self.openAction)
 		self.toolBar.addWidget(self.searchBar)
-		self.toolBar.addAction(self.startAction)
+		self.toolBar.addAction(self.searchAction)
+		#self.toolBar.addAction(self.startAction)
 		self.toolBar.addAction(self.pauseAction)
+		self.toolBar.addAction(self.settingsAction)
+		self.toolBar.setMovable(False)
+		self.searchAction.setDisabled(True)
+		self.pauseAction.setDisabled(True) # QPushButton !
+
+	def createPixmapLabel(self):
+		label = QLabel()
+		label.setEnabled(False)
+		label.setAlignment(Qt.AlignCenter)
+		label.setFrameShape(QFrame.Box)
+		label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+		label.setBackgroundRole(QPalette.Base)
+		label.setAutoFillBackground(True)
+		label.setMinimumSize(60, 60)
+		return label
 
 	def createInitialTabs(self):
-		self.tabBar = QtGui.QTabWidget()
+		self.tabBar = QTabWidget()
 		self.setCentralWidget(self.tabBar)
-		self.tabBar.addTab(QtGui.QWidget(), "Browser")
+
+		# Downloads Tab
+		#vbox.addWidget(item)
+		#btn = self.createPixmapLabel()
+		#grid.addWidget(btn)
+		list = QTableWidget()
+		item = QWidget()
+		item.setLayout(QGridLayout())
+		item.setMaximumHeight(100)
+
+		self.tabBar.addTab(list, "Downloads")
+
+		# Clipboard Tab
+		self.clipBoardList = QTableWidget()
+		self.clipBoardList.setColumnCount(3)
+		self.clipBoardList.setRowCount(3)
+		self.clipBoardList.setAlternatingRowColors(True)
+		self.clipBoardList.setDragDropMode(self.clipBoardList.InternalMove)
+		self.clipBoardList.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+		self.clipBoardList.setHorizontalHeaderLabels(["Title", "Host", "Quality"])
+		self.clipBoardList.horizontalHeader().setResizeMode(0, QHeaderView.Stretch)
+		self.clipBoardList.setSelectionBehavior(QAbstractItemView.SelectRows)
+		self.clipBoardList.verticalHeader().setDefaultSectionSize(19)
+		self.clipBoardList.verticalHeader().hide()
+		example = QTableWidgetItem("test")
+
+		self.clipBoardList.setItem(0, 0, example)
+
+		self.tabBar.addTab(self.clipBoardList, "Clipboard")
+
 
 if __name__ == '__main__':
 	import sys
-	app = QtGui.QApplication(sys.argv)
-	imageViewer = MainWindow()
-	imageViewer.show()
+
+	app = QApplication(sys.argv)
+	main_window = MainWindow()
+	main_window.show()
 	sys.exit(app.exec_())
