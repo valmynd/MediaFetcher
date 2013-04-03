a19a117f3b331bf0eb91668f8d20d0c407b29cca
rrae <rrae@localhorst.(none)>
2013-04-03 18:42:28 +0200

    initial table widgets

--- basic_gui.py
+++ basic_gui.py
@@ -13,13 +13,16 @@ class MainWindow(QMainWindow):
 		self.createActions()
 		self.createMenus()
 		self.createToolBars()
+		self.createTrayIcon()
 		self.createInitialTabs()
 		self.setWindowTitle("Media Fetcher")
 		self.resize(600, 400)
 		self.statusBar()
 		# monitor Clipboard
 		QObject.connect(QApplication.clipboard(), SIGNAL("dataChanged()"), self,
-							   SLOT("clipBoardChanged()"))
+						SLOT("clipBoardChanged()"))
+		if QSystemTrayIcon.isSystemTrayAvailable():
+			self.trayIcon.show()
 
 	@Slot()
 	def clipBoardChanged(self):
@@ -29,7 +32,9 @@ class MainWindow(QMainWindow):
 			QMessageBox.information(None, "ClipBoard", text);
 
 	def dummy(self):
-		alert(str(self.clipBoardList.getQuality(0)))
+		#alert(str(self.downLoadList.getProgress(0)))
+		#alert(str(self.clipBoardList.getQuality(0)))
+		pass
 
 	def open(self):
 		fileName = QFileDialog.getOpenFileName(self, "Open File", QDir.currentPath())
@@ -43,23 +48,26 @@ class MainWindow(QMainWindow):
 		self.statusBar().show() if self.statusBar().isHidden() else self.statusBar().hide()
 
 	def createActions(self):
-		self.openAction = QAction("&Open...", self, shortcut="Ctrl+O", triggered=self.open)
-		self.startAction = QAction("&Start All", self, triggered=self.open)
-		self.pauseAction = QAction("&Pause All", self, triggered=self.open)
-		self.exitAction = QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
+		self.openAction = QAction("&Open...", self, shortcut=QKeySequence.Open, triggered=self.open)
+		self.startAction = QAction("&Start All", self, triggered=self.dummy)
+		self.pauseAction = QAction("&Pause All", self, triggered=self.dummy)
 		self.toggleStatusBarAction = QAction("Statusbar", self, checkable=True, checked=True,
-												   triggered=self.toggleStatusBar)
+											 triggered=self.toggleStatusBar)
 		self.settingsAction = QAction("Prefere&nces", self, triggered=self.dummy)
 		self.aboutAction = QAction("About", self, triggered=self.about)
-		self.searchAction = QAction("Search", self, triggered=self.about)
+		self.searchAction = QAction("Search", self, triggered=self.dummy)
+
+		self.minimizeAction = QAction("Mi&nimize", self, triggered=self.hide)
+		self.maximizeAction = QAction("Ma&ximize", self, triggered=self.showMaximized)
+		self.restoreAction = QAction("&Restore", self, triggered=self.showNormal)
+		self.quitAction = QAction("&Quit", self, shortcut="Ctrl+Q", triggered=self.close)
 
 		self.openAction.setIcon(QIcon.fromTheme("folder-open"))
 		self.startAction.setIcon(QIcon.fromTheme("media-playback-start"))
 		self.pauseAction.setIcon(QIcon.fromTheme("media-playback-pause"))
 		self.settingsAction.setIcon(QIcon.fromTheme("emblem-system"))
-		self.exitAction.setIcon(QIcon.fromTheme("application-exit"))
+		self.quitAction.setIcon(QIcon.fromTheme("application-exit"))
 		self.searchAction.setIcon(QIcon.fromTheme("system-search"))
-		self.exitAction.setStatusTip('Exit application')
 
 	def createMenus(self):
 		self.fileMenu = QMenu("&File", self)
@@ -68,7 +76,7 @@ class MainWindow(QMainWindow):
 		self.fileMenu.addAction(self.startAction)
 		self.fileMenu.addAction(self.pauseAction)
 		self.fileMenu.addSeparator()
-		self.fileMenu.addAction(self.exitAction)
+		self.fileMenu.addAction(self.quitAction)
 
 		self.editMenu = QMenu("&Edit", self)
 		self.editMenu.addAction(self.settingsAction)
@@ -87,7 +95,7 @@ class MainWindow(QMainWindow):
 	def createToolBars(self):
 		# http://aseigo.blogspot.de/2006/08/sweep-sweep-sweep-ui-floor.html
 		self.searchBar = QLineEdit()
-		self.toolBar = self.addToolBar("Search")
+		self.toolBar = self.addToolBar("Toolbar")
 		self.toolBar.addAction(self.openAction)
 		self.toolBar.addWidget(self.searchBar)
 		self.toolBar.addAction(self.searchAction)
@@ -97,6 +105,17 @@ class MainWindow(QMainWindow):
 		self.toolBar.setMovable(False)
 		self.searchAction.setDisabled(True)
 		self.pauseAction.setDisabled(True) # QPushButton !
+
+	def createTrayIcon(self):
+		self.trayIconMenu = QMenu(self)
+		self.trayIconMenu.addAction(self.minimizeAction)
+		self.trayIconMenu.addAction(self.maximizeAction)
+		self.trayIconMenu.addAction(self.restoreAction)
+		self.trayIconMenu.addSeparator()
+		self.trayIconMenu.addAction(self.quitAction)
+
+		self.trayIcon = QSystemTrayIcon(self)
+		self.trayIcon.setContextMenu(self.trayIconMenu)
 
 	def createPixmapLabel(self):
 		label = QLabel()
@@ -115,12 +134,19 @@ class MainWindow(QMainWindow):
 
 		# Downloads Tab
 		self.downLoadList = DownloadTableWidget()
+		self.downLoadList.addRow('test1', 'Youtube', 'Avaiable')
 		self.tabBar.addTab(self.downLoadList, "Downloads")
 
 		# Clipboard Tab
 		self.clipBoardList = ClipBoardTableWidget()
+		self.clipBoardList.addRow('test1', 'Youtube', 'Avaiable', ['mp4', 'webm', 'mp3', 'ogg'], ['320p', '720p'])
 		self.tabBar.addTab(self.clipBoardList, "Clipboard")
 		self.tabBar.setCurrentIndex(1)
+
+		# Close Button for all Tabs except Downloads, Clipboard
+		self.tabBar.setTabsClosable(True)
+		self.tabBar.tabBar().tabButton(0, QTabBar.RightSide).resize(0, 0)
+		self.tabBar.tabBar().tabButton(1, QTabBar.RightSide).resize(0, 0)
 
 
 if __name__ == '__main__':
--- gui/table_widgets.py
+++ gui/table_widgets.py
@@ -1,14 +1,16 @@
-___license___ = "GPL v3"
+___license___ = 'GPL v3'
 
 from PySide.QtCore import *
 from PySide.QtGui import *
 
 
 def alert(text):
-	QMessageBox.information(None, "Alert", text)
+	QMessageBox.information(None, 'Alert', str(text))
 
 
 class _BaseTableWidget(QTableWidget):
+	_header_titles = []
+
 	def __init__(self, *args, **kwargs):
 		QTableWidget.__init__(self, *args, **kwargs)
 		# Basic Configuration
@@ -17,28 +19,38 @@ class _BaseTableWidget(QTableWidget):
 		self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 		self.setSelectionBehavior(QAbstractItemView.SelectRows)
 		self.setShowGrid(False)
+		# Init Headers
+		self.setColumnCount(len(self._header_titles))
+		self.setHorizontalHeaderLabels(self._header_titles)
 
-	def _initHeaders(self, header_titles=[]):
-		self.setColumnCount(len(header_titles))
-		self.setHorizontalHeaderLabels(header_titles)
 		# Configure Horizontal Header
 		#self.horizontalHeader().setStretchLastSection(True)
 		#self.horizontalHeader().setResizeMode(QHeaderView.Stretch)
 		self.horizontalHeader().setResizeMode(QHeaderView.ResizeToContents)
 		self.horizontalHeader().setResizeMode(0, QHeaderView.Stretch)
 		self.horizontalHeader().setHighlightSections(False)
+		self.horizontalHeader().setContextMenuPolicy(Qt.CustomContextMenu)
+		self.horizontalHeader().customContextMenuRequested.connect(self.chooseColummns)
+		self.columnMenu = QMenu()
+		for column_title in self._header_titles:
+			qa = QAction(column_title, self, checkable=True, checked=True, triggered=self.toggleColumn)
+			self.columnMenu.addAction(qa)
+
 		# Configure Vertical Header
 		self.verticalHeader().setDefaultSectionSize(22)
 		self.verticalHeader().hide()
 
+	def toggleColumn(self):
+		column_title = self.sender().text()
+		i = self._header_titles.index(column_title)
+		self.setColumnHidden(i, not self.isColumnHidden(i))
 
-class ClipBoardTableWidget(_BaseTableWidget):
-	def __init__(self):
-		_BaseTableWidget.__init__(self)
-		self._initHeaders(["Title", "Host", "Quality"])
-		self.addRow("test1", "Youtube", ["320p", "720p"])
-		#self.addRow("test2", "Youtube", ["320p"])
-		#self.addRow("test3", "Youtube", ["320p"])
+	def chooseColummns(self, pos):
+		globalPos = self.mapToGlobal(pos)
+		selectedItem = self.columnMenu.exec_(globalPos)
+
+	def getStatus(self, num_row):
+		return self.item(num_row, 2).text()
 
 	def getTitle(self, num_row):
 		return self.item(num_row, 0).text()
@@ -46,25 +58,52 @@ class ClipBoardTableWidget(_BaseTableWidget):
 	def getHost(self, num_row):
 		return self.item(num_row, 1).text()
 
-	def getQuality(self, num_row):
-		return self.cellWidget(num_row, 2).currentText()
-
-	def addRow(self, title='', host='', quality_options=[]):
+	def addRow(self, title, host, status):
 		r = self.rowCount()
 		self.setRowCount(r + 1)
 		title_item = QTableWidgetItem(title)
 		host_item = QTableWidgetItem(host)
 		host_item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-		quality_combobox = QComboBox()
-		#quality_combobox.setStyleSheet("border:0")
-		for option in quality_options:
-			quality_combobox.addItem(option)
+		status_item = QTableWidgetItem(status)
+		status_item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
 		self.setItem(r, 0, title_item)
 		self.setItem(r, 1, host_item)
-		self.setCellWidget(r, 2, quality_combobox)
+		self.setItem(r, 2, status_item)
+		return r
+
+
+class ClipBoardTableWidget(_BaseTableWidget):
+	_header_titles = ['Title', 'Host', 'Status', 'Format', 'Quality']
+
+	def getFormat(self, num_row):
+		return self.cellWidget(num_row, 3).currentText()
+
+	def getQuality(self, num_row):
+		return self.cellWidget(num_row, 4).currentText()
+
+	def addRow(self, title='', host='', status='', format_options=[], quality_options=[]):
+		r = _BaseTableWidget.addRow(self, title, host, status)
+		format_combobox = QComboBox()
+		quality_combobox = QComboBox()
+		#quality_combobox.setStyleSheet('border:0')
+		for option in format_options:
+			format_combobox.addItem(option)
+		for option in quality_options:
+			quality_combobox.addItem(option)
+		self.setCellWidget(r, 3, format_combobox)
+		self.setCellWidget(r, 4, quality_combobox)
+		return r
+
 
 class DownloadTableWidget(_BaseTableWidget):
-	def __init__(self):
-		_BaseTableWidget.__init__(self)
-		self._initHeaders(["Title", "Host", "Progress"])
-		#self.addRow("test1", "Youtube", ["320p", "720p"])
+	_header_titles = ['Filename', 'Host', 'Status', 'Progress']
+
+	def getProgress(self, num_row):
+		return self.cellWidget(num_row, 3).value()
+
+	def addRow(self, title='', host='', status='', progress=0):
+		r = _BaseTableWidget.addRow(self, title, host, status)
+		progress_widget = QProgressBar()
+		progress_widget.setValue(progress)
+		self.setCellWidget(r, 3, progress_widget)
+		return r
